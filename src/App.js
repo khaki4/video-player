e7a24507e7abd49877204c91ee90149aacc259d6
jiwonjung <4dakto@gmail.com>
2019-12-13 22:52:55 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMachine } from '@xstate/react';
+import { Machine, assign } from 'xstate';
+import { percentage, minutes, seconds } from "./utils";
+
+const videoMachine = new Machine({
+  id: 'videoMachine',
+  initial: 'loading',
+  context: {
+    video: null,
+    duration: 0,
+    elapsed: 0
+  },
+  states: {
+    loading: {
+      on: {
+        LOADED: 'ready',
+        FAIL: 'failure'
+      }
+    },
+    ready: {
+      initial: 'paused',
+      states: {
+        paused: {
+          on: {
+            PLAY: 'playing'
+          }
+        },
+        playing: {
+          on: {
+            PAUSE: 'paused',
+            END: 'ended',
+            TIMING: 'playing'
+          }
+        },
+        ended: {
+          on: {
+            PLAY: 'playing'
+          }
+        }
+      }
+    },
+    failure: { type: 'final'}
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video controls>
+        <source src="/test.mp4" type="video/mp4" />
+      </video>
+
+      <div>
+        <ElapsedBar elapsed={0} duration={0} />
+        <Buttons />
+        <Timer elapsed={0} duration={0} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Buttons = () => {
+  return <button onClick={() => {}}>Play / Pause</button>
+};
+
+const ElapsedBar = ({ elapsed, duration }) => (
+  <div className="elapsed">
+    <div
+      className="elapsed-bar"
+      style={{ width: `${percentage(duration, elapsed)}%` }}
+    />
+  </div>
+);
+
+const Timer = ({ elapsed, duration }) => (
+  <span className="timer">
+    {minutes(elapsed)}:{seconds(elapsed)} of {minutes(duration)}:
+    {seconds(duration)}
+  </span>
+);
